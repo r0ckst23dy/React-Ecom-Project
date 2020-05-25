6109a7c62eb218de3eec3c91262a67d3da565b43
r0ckst34dy <r0ck1ngst34dy@outlook.com>
2020-05-25 12:12:19 -0400

    created a title component, styled and brought in several fonts

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SignInForm from '../../style/auth/signinform';
+import PageTitle from '../pageTitle';
 
 
 
@@ -12,6 +13,7 @@ class SignIn extends Component {
     render() {
         return ( 
             <div className= 'sign-in'> 
+                <PageTitle className='sign-in__page-title' title='Login'/>
                 <SignInForm onSubmit={this.onSubmit} className='sign-in__form'/>
             </div>
         )
