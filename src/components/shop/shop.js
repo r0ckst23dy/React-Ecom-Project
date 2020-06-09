35f67cabf3809c9ec931469383b02fec9c18b85b
r0ckst34dy <r0ck1ngst34dy@outlook.com>
2020-06-08 21:16:39 -0400

    built shopProduct, Quantity, and GreenPriceTag component and added styling to GreenPriceTag component

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
+import ShopProduct from '../../style/shop/shopProduct';
 
 class Shop extends Component { 
     componentDidMount() {
@@ -34,25 +35,17 @@ class Shop extends Component {
     render() { 
         return(
             <div className='shop'>
-            <ShopSearchBar className='shop__search-bar' onSubmit={this.onSubmit} />
-            <div className= 'shop_products' >
-                {
-                    this.props.filteredProducts.map(product => {
-                        return (
-                            <div key={product._id} className='shop-product'>
-                                <div className='shop-product__title'>
-                                    {product.title}
-                                </div>
-                                <div className='shop-product__description'>
-                                    {product.description}
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-            {/* cart button */}
-                shop...
+                <ShopSearchBar className='shop__search-bar' onSubmit={this.onSubmit} />
+                <div className= 'shop_products' >
+                    {
+                        this.props.filteredProducts.map(product => {
+                            return (
+                                <ShopProduct {...product} key= {product._id}/>
+                            )
+                        })
+                    }
+                </div>
+                {/* cart button */}
             </div>
         )
     }
